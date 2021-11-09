3c18a9d0a94b65f7ba18eee692bddb891321e8ac
Binh Trinh An (VN) <Binh.TrinhA@homecredit.vn>
2021-11-09 23:49:06 +0700

    first commit

---
+++ conn.py
@@ -0,0 +1,7 @@
+import cx_Oracle
+
+db_conn = cx_Oracle.connect('TRINGUYENH/peakvietnam@//103.163.214.72:1521/XEPDB1', encoding = "UTF-8")
+cursor = db_conn.cursor()
+cursor.execute('SELECT * FROM T_D_ACCOUNT_TYPE')
+
+print(cursor.fetchall())
