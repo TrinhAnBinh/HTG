0235896ffc92a4b6946dc2edbc3f074605927688
Binh Trinh An (VN) <Binh.TrinhA@homecredit.vn>
2021-11-22 14:07:41 +0700

    Update OCI link

--- conn.py
+++ conn.py
@@ -1,6 +1,6 @@
 import cx_Oracle
 
-db_conn = cx_Oracle.connect('TRINGUYENH/peakvietnam@//103.163.214.72:1521/XEPDB1', encoding = "UTF-8")
+db_conn = cx_Oracle.connect('TRINGUYENH/password_oracle@//103.163.214.72:1521/XEPDB1', encoding = "UTF-8")
 cursor = db_conn.cursor()
 cursor.execute('SELECT * FROM T_D_ACCOUNT_TYPE')
 
